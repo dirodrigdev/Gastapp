335c1db6efb9f24f22b7ac7b15273cfac489e191
dirodrigdev <diegorp.1978@gmail.com>
2025-12-05 02:00:35 +0100

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// TUS CREDENCIALES REALES
+// TUS CREDENCIALES REALES (Las que ya pegaste antes)
 const firebaseConfig = {
   apiKey: "AIzaSyDM1oC6SBU_kRrLSJq0P7P5-NUGWSRVnh0",
   authDomain: "duofin-c1894.firebaseapp.com",
@@ -12,7 +12,6 @@ const firebaseConfig = {
   appId: "1:560986741892:web:3d2b4aebf15872f4a536d1"
 };
 
-// Inicializamos la conexión
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
